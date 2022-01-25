56d5deb9359ce1c6b470ca18dc8a0777de0c4ebd
Paloma Lins <palomalimadl@gmail.com>
2022-01-25 14:58:15 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function mensagem(){
+    console.log("Olá a todos!")
+}
+mensagem()
+
+function name(nome,age,estilo) {
+    console.log(`Meu nome é ${nome}, eu tenho ${age} meu esilo musical favorito é ${estilo}`)
+}
+name("Paloma", 16, "J-Pop")
+
+function favoritos(filme, musica ) {
+    console.log(`Meu filme favorito é ${filme} e minha música favorita é ${musica}`)
+}
+favoritos("Encanto", "廻廻奇譚 (Kaikai Kitan)")
+
+function triplo(x){
+    return x * 3
+}
+console.log(triplo(7))
